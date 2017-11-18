36c1e7f4d57c858edcda89df97da1b6bdc62e6a5
luisfqv2194 <luisfqv94@gmail.com>
2017-11-18 13:45:45 -0600

    Termino tutorial 18

--- webdrive/src/main/java/webdrive/controllers/LogInController.java
+++ webdrive/src/main/java/webdrive/controllers/LogInController.java
@@ -1,13 +1,37 @@
 package webdrive.controllers; // Para leer los controladores el paquete debe ser un subpaquete del paquete principal
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import webdrive.business.User;
+import webdrive.services.UserService;
+
 @RestController
 public class LogInController {
+	
+	@Autowired
+	private UserService userService;
 
 	@RequestMapping("/login") // Todos los métodos
 	public String sayHi() {
 		return "hi";
 	}
+	
+	@RequestMapping("/login/{username}") // Todos los métodos
+	public User getUser(@PathVariable String username) {
+		
+		return userService.getUser(username);
+	}
+	
+	@RequestMapping("/list") // Todos los métodos
+	public List<User> sayList() {
+		return userService.getUsers();
+	}
+	
+	
 }
--- webdrive/src/main/java/webdrive/services/UserService.java
+++ webdrive/src/main/java/webdrive/services/UserService.java
@@ -1,5 +1,33 @@
 package webdrive.services;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import webdrive.business.User;
+
+@Service   //Debe ser un singleton
 public class UserService {
+	
+	private List<User> users = Arrays.asList(
+			new User("user1","pass1",1), 
+			new User("user2","pass2",2), 
+			new User("user3","pass1",3));
+
+	public List<User> getUsers() {
+		return users;
+	}
+
+	public void setUsers(List<User> users) {
+		this.users = users;
+	}
+	
+	public User getUser(String username) {
+		return users.stream().filter(user -> user.getUsername().equals(username)).findFirst().get();
+		
+	}
+	
+	
 
 }
